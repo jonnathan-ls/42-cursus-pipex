3c8e3b04629702c600d78242886bde223d2d7902
Jonnathan Lacerda Santos <jlacerda@student.42sp.org.br>
2025-01-04 18:11:04 -0300

    feat: add initial configuration main program and libraries

---
+++ includes/pipex.h
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   pipex.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jlacerda <jlacerda@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/01/04 17:25:59 by jlacerda          #+#    #+#             */
+/*   Updated: 2025/01/04 18:03:57 by jlacerda         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PIPEX_H
+# define PIPEX_H
+# include <fcntl.h>
+# include <stdlib.h>
+# include <unistd.h>
+# include "macros.h"
+# include "structs.h"
+
+#endif
